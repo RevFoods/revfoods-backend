d10f0e772c36ea9fd19824347b1e2c3dbd7fb150
“Akshay <akshay.sargar17@vit.edu>
2021-10-05 09:41:19 +0530

    Added Revfood Backend Project Structure

---
+++ src/main/java/com/app/revfoodsbackend/RevfoodsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.revfoodsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevfoodsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevfoodsBackendApplication.class, args);
+    }
+
+}
