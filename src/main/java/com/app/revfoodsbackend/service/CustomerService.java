515ab75799990337343d0a83d9ead7f433dc0be0
Akshay Sargar <akshay.sargar17@vit.edu>
2021-10-06 20:46:27 +0530

    Update CustomerService.java

--- src/main/java/com/app/revfoodsbackend/service/CustomerService.java
+++ src/main/java/com/app/revfoodsbackend/service/CustomerService.java
@@ -5,10 +5,9 @@ import java.util.List;
 import com.app.revfoodsbackend.model.Customer;
 
 public interface CustomerService {
-	 Customer addCustomer(Customer customer);
-	 Customer updateCustomer(Customer customer);
-	 List<Customer> getAllCustomers();
-     Customer getCustomerByCustomerId(int customerId);
-	 void deleteCustomer(int customerId); 
-
+	Customer addCustomer(Customer customer);
+	Customer updateCustomer(Customer customer);
+	List<Customer> getAllCustomers();
+     	Customer getCustomerByCustomerId(int customerId);
+	void deleteCustomer(int customerId); 
 }
