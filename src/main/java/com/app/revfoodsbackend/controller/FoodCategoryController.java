73ad03c9f384e9e0abc6dbabe523f3514f64607b
Akshay Sargar <akshay.sargar17@vit.edu>
2021-10-07 20:17:47 +0530

    Update FoodCategoryController.java

--- src/main/java/com/app/revfoodsbackend/controller/FoodCategoryController.java
+++ src/main/java/com/app/revfoodsbackend/controller/FoodCategoryController.java
@@ -2,7 +2,6 @@ package com.app.revfoodsbackend.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,50 +12,38 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.app.revfoodsbackend.model.FoodCategory;
 import com.app.revfoodsbackend.service.FoodCategoryService;
-
 
 @RestController
 @CrossOrigin
 public class FoodCategoryController {
-	
 
 	@Autowired
-	private FoodCategoryService service;
+	private FoodCategoryService foodCategoryService;
 	
 	@GetMapping("/foodCategories")
 	public List<FoodCategory> getAllFoodCategories() {
-	
-		return service.getAllFoodCategories();
+		return foodCategoryService.getAllFoodCategories();
 	}
 	
 	@PostMapping("/foodCategory")
 	public FoodCategory addFoodCategory(@RequestBody FoodCategory foodCategory) {
-		
-			return service.addFoodCategory(foodCategory);
+		return foodCategoryService.addFoodCategory(foodCategory);
 	}
 	
 	@PutMapping("/foodCategory")
 	public FoodCategory updateFoodCategory(@RequestBody FoodCategory foodCategory) {
-
-		return service.updateFoodCategory(foodCategory);
-
+		return foodCategoryService.updateFoodCategory(foodCategory);
 	}
 	
 	@GetMapping("/foodCategory/{foodCategoryId}")
 	public FoodCategory getFoodCategoryById(@PathVariable int foodCategoryId) {
-			return service.getFoodCategoryById(foodCategoryId);	
-		
+		return foodCategoryService.getFoodCategoryById(foodCategoryId);		
 	}
 
 	@DeleteMapping("/foodCategory/{foodCategoryId}")
 	public void deleteFoodCategory(@PathVariable int foodCategoryId) {
-
-		service.deleteFoodCategory(foodCategoryId);
+		foodCategoryService.deleteFoodCategory(foodCategoryId);
 	}
-
-	
-	
 }
