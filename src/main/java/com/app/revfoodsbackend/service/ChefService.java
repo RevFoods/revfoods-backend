d3e4e882caff54bde65514079c1e3de36c88d581
Akshay Sargar <akshay.sargar17@vit.edu>
2021-10-07 19:59:50 +0530

    Update ChefService.java

--- src/main/java/com/app/revfoodsbackend/service/ChefService.java
+++ src/main/java/com/app/revfoodsbackend/service/ChefService.java
@@ -5,15 +5,9 @@ import java.util.List;
 import com.app.revfoodsbackend.model.Chef;
 
 public interface ChefService {
-
 	Chef addChef(Chef chef);
-
 	Chef updateChef(Chef chef);
-
 	void deleteChef(int chefId);
-
 	List<Chef> getAllChefs();
-
 	Chef getChefById(int chefId);
-
 }
