1f66129b05f376765ff1588f529b8b190cd93157
Akshay Sargar <akshay.sargar17@vit.edu>
2021-10-08 09:36:24 +0530

    Update SupervisorService.java

--- src/main/java/com/app/revfoodsbackend/service/SupervisorService.java
+++ src/main/java/com/app/revfoodsbackend/service/SupervisorService.java
@@ -7,7 +7,7 @@ import com.app.revfoodsbackend.model.Supervisor;
 public interface SupervisorService {
 	Supervisor addSupervisor(Supervisor supervisor);
 	void deleteSupervisor(int supervisorId);
-	List<Supervisor> getSupervisors();
+	List<Supervisor> getAllSupervisors();
 	Supervisor getSupervisorBySupervisorId(int supervisorId);
 	Supervisor updateSupervisor(Supervisor supervisor);
 }
