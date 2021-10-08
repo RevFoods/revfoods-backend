4d3a3cae523d997d853abf40111ebbf4c8ee904d
Akshay Sargar <akshay.sargar17@vit.edu>
2021-10-08 09:37:21 +0530

    Update SupervisorServiceImpl.java

--- src/main/java/com/app/revfoodsbackend/service/impl/SupervisorServiceImpl.java
+++ src/main/java/com/app/revfoodsbackend/service/impl/SupervisorServiceImpl.java
@@ -8,32 +8,35 @@ import org.springframework.stereotype.Service;
 import com.app.revfoodsbackend.model.Supervisor;
 import com.app.revfoodsbackend.repository.SupervisorRepository;
 import com.app.revfoodsbackend.service.SupervisorService;
+
 @Service
 public class SupervisorServiceImpl implements SupervisorService {
 	
 	@Autowired
 	private SupervisorRepository supervisorRepository;
+	
 	@Override
 	public Supervisor addSupervisor(Supervisor supervisor) {
-		
 		return supervisorRepository.save(supervisor);
 	}
+	
 	@Override
 	public void deleteSupervisor(int supervisorId) {
 		supervisorRepository.deleteById(supervisorId);
 	}
+	
 	@Override
-	public List<Supervisor> getSupervisors(){
+	public List<Supervisor> getAllSupervisors(){
 		return supervisorRepository.findAll();
 	}
+	
 	@Override
 	public Supervisor updateSupervisor(Supervisor supervisor) {
 		return supervisorRepository.save(supervisor);
 	}
+	
 	@Override
 	public Supervisor getSupervisorBySupervisorId(int supervisorId) {
 		return supervisorRepository.findById(supervisorId).get();
 	}
-	
-	
 }
