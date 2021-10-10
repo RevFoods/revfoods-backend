ccc1316b023e6e6b0e027342073e7754417e1d2e
sumansuruchi <suruchiramkrishnamishra@gmail.com>
2021-10-10 17:30:11 +0530

    TASK-55 Added Customer, Customer Table, Cart And, Feedback Test Cases

---
+++ src/test/java/com/app/revfoodsbackend/CartServiceTest.java
@@ -0,0 +1,125 @@
+package com.app.revfoodsbackend;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.app.revfoodsbackend.model.Cart;
+import com.app.revfoodsbackend.model.Customer;
+import com.app.revfoodsbackend.model.Food;
+import com.app.revfoodsbackend.repository.CartRepository;
+import com.app.revfoodsbackend.repository.CustomerRepository;
+import com.app.revfoodsbackend.repository.FoodRepository;
+import com.app.revfoodsbackend.service.CartService;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class CartServiceTest {
+	
+	@Autowired
+	CartService cartService;
+	
+	@MockBean
+	CartRepository cartRepository;
+	
+	@MockBean
+	FoodRepository foodRepository;
+	
+	@MockBean
+	CustomerRepository customerRepository;
+	
+	Cart cart;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		cart = new Cart();
+		cart.setCartQuantity(4);
+	}
+	
+	@Test
+	@Order(1)
+	public void addCartTest() {
+		when(cartRepository.save(cart)).thenReturn(cart);
+		assertEquals(cart, cartService.addCart(cart));
+	}
+	
+	@Test
+	@Order(2)
+	public void getAllCartsTest() {
+		when(cartRepository.findAll()).thenReturn(Stream.of(new Cart(),new Cart()).collect(Collectors.toList()));
+		assertEquals(2, cartService.getAllCarts().size());
+	}
+	
+	@Test
+	@Order(3)
+	public void getCartByCartIdTest() {
+		when(cartRepository.getById(cart.getCartId())).thenReturn(cart);
+		assertEquals(cart,cartService.getCartByCartId(cart.getCartId()));
+	}
+	
+	@Test
+	@Order(4)
+	public void addFoodAndCustomerToCartTest() {
+		Customer customer=new Customer();
+		customer.setCustomerName("Akshay");
+		customer.setCustomerEmail("Akshay@gmail.com");
+		customer.setCustomerPhone(3456778893l);
+		
+		Customer newCustomer = customerRepository.save(customer);
+		cart.setCustomer(newCustomer);
+		
+		Food food = new Food();
+		food.setFoodName("Momo");
+		food.setFoodPrepTime(20);
+		food.setFoodPrice(50.00);
+		food.setFoodAvatar("Img");
+		food.setFoodType("veg");
+		food.setFoodStatus(true);
+		
+		Food newFood = foodRepository.save(food);
+		cart.setFood(newFood);
+		 //Cart newCart=cartRepository.save(cart);
+		
+		
+		when(customerRepository.findById(customer.getCustomerId())).thenReturn(Optional.of(customer));
+		when(foodRepository.findById(food.getFoodId())).thenReturn(Optional.of(food));
+		when(cartRepository.save(cart)).thenReturn(cart);
+		assertEquals(cart, cartService.addFoodAndCustomerToCart(food.getFoodId(), customer.getCustomerId(), cart.getCartQuantity()));
+		
+	}
+	
+	@Test
+	@Order(5)
+	public void getAllCartsByCustomerIdTest() {
+		Customer customer=new Customer();
+		customer.setCustomerName("Akshay");
+		customer.setCustomerEmail("Akshay@gmail.com");
+		customer.setCustomerPhone(3456778893l);
+		when(customerRepository.findById(customer.getCustomerId())).thenReturn(Optional.of(customer));
+		when(cartRepository.findCartsByCustomer(customer)).thenReturn(Stream.of(new Cart(),new Cart()).collect(Collectors.toList()));
+		assertEquals(2, cartService.getAllCartsByCustomerId(cart.getCartId()).size());
+	}
+	
+	@Test
+	@Order(6)
+	public void deleteCartTest() {
+		when(cartRepository.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+		cartService.deleteCart(cart.getCartId());
+		verify(cartRepository).deleteById(cart.getCartId());
+	}
+	
+}
---
+++ src/test/java/com/app/revfoodsbackend/FeedbackServiceTest.java
@@ -0,0 +1,89 @@
+package com.app.revfoodsbackend;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.app.revfoodsbackend.model.Cart;
+import com.app.revfoodsbackend.model.Feedback;
+import com.app.revfoodsbackend.repository.FeedbackRepository;
+import com.app.revfoodsbackend.service.FeedbackService;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class FeedbackServiceTest {
+	
+	@Autowired
+	FeedbackService feedbackService;
+	
+	@MockBean
+	FeedbackRepository feedbackRepository;
+	
+	Feedback feedback;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		feedback = new Feedback();
+		feedback.setFeedback("delicious food");
+		feedback.setAmbience(4);
+		feedback.setHygiene(4);
+		feedback.setService(4);
+	}
+
+	
+	
+	@Test
+	@Order(1)
+	public void addFeedbackTest() {
+		when(feedbackRepository.save(feedback)).thenReturn(feedback);
+		assertEquals(feedback, feedbackService.addFeedback(feedback));
+	}
+	
+	@Test
+	@Order(2)
+	public void getAllFeedbacksTest() {
+		when(feedbackRepository.findAll()).thenReturn(Stream.of(new Feedback(),new Feedback()).collect(Collectors.toList()));
+		assertEquals(2, feedbackService.getAllFeedbacks().size());
+	}
+	
+	@Test
+	@Order(3)
+	public void getFeedbackByIdTest() {
+		int id=1;
+		when(feedbackRepository.findById(id)).thenReturn(Optional.of(feedback));
+		assertEquals(feedback,feedbackService.getFeedbackById(id));
+	}
+	
+	@Test
+	@Order(4)
+	public void updateFeedbackTest() {
+		feedback.setAmbience(5);
+		when(feedbackRepository.save(feedback)).thenReturn(feedback);
+		assertNotEquals("Navi Mumbai", feedbackService.updateFeedback(feedback));
+	}
+	
+	@Test
+	@Order(5)
+	public void deleteFeedbackTest() {
+		when(feedbackRepository.findById(feedback.getFeedbackId())).thenReturn(Optional.of(feedback));
+		feedbackService.deleteFeedback(feedback.getFeedbackId());
+		verify(feedbackRepository).deleteById(feedback.getFeedbackId());
+	}
+		
+
+}
